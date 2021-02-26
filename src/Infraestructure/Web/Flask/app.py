8a248248aa92341f5684e405d6c8d8aadc99ab19
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2021-02-26 01:47:35 -0600

    Refactor class registermoduleblueprint move to new file

--- src/Infraestructure/Web/Flask/app.py
+++ src/Infraestructure/Web/Flask/app.py
@@ -1,21 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
+from src.Infraestructure.Web.Flask.register_modules_blueprint import RegisterModulesBlueprint
 from src.Infraestructure.Web.Flask.rest.users.register_blueprint import UsersRegisterBlueprint
 
-
-class RegisterModulesBlueprint:
-    def __init__(self, app):
-        self.app = app
-        self.modules_blueprints = []
-
-    def append_module(self, module_blueprint):
-        self.modules_blueprints.append(module_blueprint)
-
-    def register(self):
-        for module in self.modules_blueprints:
-            self.app = module.register(self.app)
-
-        return self.app
         
 def create_app():
     app = Flask(__name__)
