dae20ff718d8b532cf69e6f5e3d55ea71ef5a885
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2021-03-18 19:15:37 -0600

    Add route for components and empty example template

--- src/Infraestructure/Web/Angular/src/app/log-in/log-in.module.ts
+++ src/Infraestructure/Web/Angular/src/app/log-in/log-in.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogInComponent } from './log-in/log-in.component';
+import { RouterModule, Routes } from '@angular/router';
 
 
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LogInComponent
+  }
+];
 
 @NgModule({
-  declarations: [LogInComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    LogInComponent
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class LogInModule { }
