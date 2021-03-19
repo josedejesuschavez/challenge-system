6fbb2fd336b362f2701e287ef7a09b9d0e3a23a2
Jose de Jesus Chavez <chavezjal06@hotmail.com>
2021-03-19 02:32:41 -0600

    Resolve bug for use components in modules

--- src/Infraestructure/Web/Angular/src/app/pits-dashboard/pits-dashboard.module.ts
+++ src/Infraestructure/Web/Angular/src/app/pits-dashboard/pits-dashboard.module.ts
@@ -13,8 +13,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), TeamMemberReadOnlyComponent],
-  exports: [RouterModule, TeamMemberReadOnlyComponent],
-  declarations: [TeamMemberReadOnlyComponent]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+  declarations: [
+    PitsDashboardComponent,
+    TeamMemberReadOnlyComponent
+  ]
 })
 export class PitsDashboardModule { }
